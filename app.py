0e0f05825c27b51f6d512ec41d5caede8b1accba
Chinar Dankhara <101128521+chinardankhara@users.noreply.github.com>
2023-03-08 20:02:00 -0500

    image display works

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import os
 import streamlit as st
+import cv2
 
-st.set_page_config(page_title="Annotation App", page_icon=":pencil2:", layout="wide", initial_sidebar_state="collapsed")
-
-def display_home():
-    st.markdown("<h1 style='text-align: center; color: black;'>Annotation App</h1>", unsafe_allow_html=True)
-    display_utils()
-    display_image_names()
-
-    #display the correct image in the main window
-    if st.session_state.active_image != "":
-        st.image("data/images/"+st.session_state.active_image, use_column_width=True)
-        st.write(st.session_state.active_image) #TODO: UNTESTED
+st.set_page_config(page_title="Annotation App", page_icon=":pencil2:", layout="wide", initial_sidebar_state="auto")
 
 def display_image_names():
-    st.session_state["active_image"] = ""
     filelist=[]
     for root, dirs, files in os.walk("data/images"):
         for file in files:
             filename=os.path.join(root, file)
             #remove the prefix of the path
             filename=filename.replace("data/images/", "")
+            filename= filename.replace(".jpg", "")
             filelist.append(filename)
             #remove DS_Store
             if filename == ".DS_Store":
@@ -29,11 +20,12 @@ def display_image_names():
     def update_active_image(i):
         st.session_state.active_image = i
         print(st.session_state.active_image)
-        st.write(st.session_state.active_image)
+        #st.image("data/images/"+st.session_state.active_image + '.jpg', use_column_width=True)
 
     with st.sidebar:
         for i in filelist:
             st.button(i, on_click = update_active_image, args = (i,))
+
 def display_utils():
     remove_top_space_style = """
         <style>
@@ -57,4 +49,18 @@ def display_utils():
     
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
+def display_home():
+    st.markdown("<h1 style='text-align: center; color: black;'>Annotation App</h1>", unsafe_allow_html=True)
+    if "active_image" not in st.session_state:
+        st.session_state["active_image"] = ""
+    if st.session_state["active_image"] != "":
+        st.image("data/images/"+st.session_state["active_image"] + '.jpg', use_column_width=True)
+
+    display_utils()
+    #st.session_state["active_image"] = ""
+    display_image_names()
+
 display_home()
+
+
+
